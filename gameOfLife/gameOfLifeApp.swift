9618251ba34ef9c55de4bcaea0afed79c8ef670e
Tony Zhang <tony.j.h.zhang@gmail.com>
2021-04-17 18:06:00 -0400

    Initial Commit

---
+++ gameOfLife/gameOfLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  gameOfLifeApp.swift
+//  gameOfLife
+//
+//  Created by Tony Zhang on 4/17/21.
+//
+
+import SwiftUI
+
+@main
+struct gameOfLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
